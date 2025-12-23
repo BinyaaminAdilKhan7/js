372f02d889345f64118706ce695e52c43a2e569d
BinyaaminAdilKhan7 <binyaaminadilkhan@gmail.com>
2025-12-23 07:05:57 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID=11223344
+let accountemail="bin@gits.com"
+var accountpassword="passcode"
+accountcity="Bangalore"
+let accountstate;
+// accountID = 20 (not allowed)
+accountemail="git@Bins.com"
+accountpassword="codepass"
+accountcity="Jaipur"
+console.log([accountID, accountemail, accountpassword, accountcity]);
