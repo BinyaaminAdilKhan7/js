b4cc6f90f165f2237ab4b8ffffcb1cbbb88b466d
BinyaaminAdilKhan7 <binyaaminadilkhan@gmail.com>
2025-12-26 06:29:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+ "use strict";  // treat all js code as newer version
+
+ //alert(4+8) // we are using nodejs, not browser
+
+/*console.log(4
+    + 8) 
+    code readability should be high(bole tohhh eassy)*/
+    
+    console.log("Binyaamin");
+
+    let name="Binyaamin"
+    let age =18
+    let isloggedin=false
+    let state;
+
+    // number=> 2 to power 53
+    // bigint
+    // string =>""
+    // boolean => true or false 
+    // null => standalone value
+    // undefined =>
+    // symbol (react)=> Unique
+
+
+    // objects
+// null is an object
+     console.log(typeof null);  // output is object 
+     console.log(typeof undefined);  //output is undefined
+     
+
+    
