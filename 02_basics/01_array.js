3ca43fa7b514c7604e1801700f46cd0ff8a84568
BinyaaminAdilKhan7 <binyaaminadilkhan@gmail.com>
2025-12-29 05:22:07 +0000

    Arrays

---
+++ 02_basics/01_array.js
@@ -0,0 +1,39 @@
+// array
+const myArr = [0,1,2,3,4,5]
+// const myHeroes = ["khalid","Umar","Ali","Abu bakr"]
+ 
+// const myArr2 = new Array(9,8,7,6,5)
+// console.log(myHeroes[1],myArr[3],myArr2[2]);
+
+// Array methods
+// myArr.push(6,7)  // adds the value to the Array
+//  myArr.pop() // removes the last element from the Array.
+
+// myArr.unshift(9) //adds the value to the start of Array.
+// myArr.shift()    //deletes the first value from the Array.
+
+// console.log(myArr.includes(0));// 'includes' gives output in 'boolean' type.
+// console.log(myArr.indexOf(9)); // displays the index .
+
+
+
+// const newArr = myArr.join() // 'join' converts an Array into String.
+
+//  console.log(myArr);
+//  console.log(typeof newArr);
+//  console.log(newArr);
+ 
+// slice , splice 
+
+// console.log("A",myArr);
+
+// const myn1= myArr.slice(2,5)
+// console.log(myn1);
+// console.log("B",myArr);
+
+// const myn2 = myArr.splice(2,5)// splice manipulates the array. deletes the elements mentioned in paranthesis from the Array.
+//     console.log("C",myArr);
+//     console.log(myn2);
+
+
+ 
